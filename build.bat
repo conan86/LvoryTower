f114789df7745164b49fd7d58d593f4e8e6bad7a
Conan Sun <125001930@qq.com>
2015-11-06 00:12:09 +0800

    update the mvn build.bat

--- build.bat
+++ build.bat
@@ -1,2 +1,1 @@
-mvn clean install dependency:sources generate-sources eclipse:eclipse -Dwtpversion=2.0 -Dmaven.test.skip=true
-pause
+mvn clean install dependency:sources generate-sources eclipse:eclipse -Dmaven.test.skip=true -Dwtpversion=2.0
