9068314aeaec55b88a01961ac51f4f7fc224ba64
Conan Sun <125001930@qq.com>
2015-11-09 23:58:58 +0800

    Add new module try to use the mybatis to do the mapping.

---
+++ src/main/java/com/tusk/lvoryTower/module/User.java
@@ -0,0 +1,106 @@
+package com.tusk.lvoryTower.module;
+
+import java.util.Date;
+
+public class User {
+	private int userId;
+	private String username;
+	private String password;
+	private int birthday;
+	private String idNumber;
+	private String idCardPhotoPath;
+	private String realName;
+	private int shoolId;
+	private int departmentId;
+	private String photoPath;
+	private Date registerDate;
+
+	public int getUserId() {
+		return userId;
+	}
+
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public int getBirthday() {
+		return birthday;
+	}
+
+	public void setBirthday(int birthday) {
+		this.birthday = birthday;
+	}
+
+	public String getIdNumber() {
+		return idNumber;
+	}
+
+	public void setIdNumber(String idNumber) {
+		this.idNumber = idNumber;
+	}
+
+	public String getIdCardPhotoPath() {
+		return idCardPhotoPath;
+	}
+
+	public void setIdCardPhotoPath(String idCardPhotoPath) {
+		this.idCardPhotoPath = idCardPhotoPath;
+	}
+
+	public String getRealName() {
+		return realName;
+	}
+
+	public void setRealName(String realName) {
+		this.realName = realName;
+	}
+
+	public int getShoolId() {
+		return shoolId;
+	}
+
+	public void setShoolId(int shoolId) {
+		this.shoolId = shoolId;
+	}
+
+	public int getDepartmentId() {
+		return departmentId;
+	}
+
+	public void setDepartmentId(int departmentId) {
+		this.departmentId = departmentId;
+	}
+
+	public String getPhotoPath() {
+		return photoPath;
+	}
+
+	public void setPhotoPath(String photoPath) {
+		this.photoPath = photoPath;
+	}
+
+	public Date getRegisterDate() {
+		return registerDate;
+	}
+
+	public void setRegisterDate(Date registerDate) {
+		this.registerDate = registerDate;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+}
---
+++ src/main/java/com/tusk/lvoryTower/utils/EncryptUtil.java
@@ -0,0 +1,44 @@
+package com.tusk.lvoryTower.utils;
+
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.password.StandardPasswordEncoder;  
+  
+/** 
+ * @author XUYI 
+ * Spring Security 3.1 PasswordEncoder 
+ */  
+public class EncryptUtil {  
+    //从配置文件中获得  
+    private static final String SITE_WIDE_SECRET = "my-secret-key-lvory-tower";  
+    private static final PasswordEncoder encoder = new StandardPasswordEncoder(  
+       SITE_WIDE_SECRET);  
+   
+    public static String encrypt(String rawPassword) {  
+         return encoder.encode(rawPassword);  
+    }  
+   
+    public static boolean match(String rawPassword, String password) {  
+         return encoder.matches(rawPassword, password);  
+    }
+    
+    public static String encryptByMD5(String str) throws NoSuchAlgorithmException {
+	    MessageDigest md = MessageDigest.getInstance("MD5");
+	    md.update(str.getBytes());
+	    return new BigInteger(1, md.digest()).toString(16);
+    }
+      
+    public static void main(String[] args) throws NoSuchAlgorithmException {
+    	String originalPassword = "abcdwerasdfasf12341235";
+    	String passwordEncryotByMD5 = encryptByMD5(originalPassword);
+    	System.out.println(passwordEncryotByMD5);
+    	String s1 = EncryptUtil.encrypt(passwordEncryotByMD5);
+    	System.out.println(s1);
+    	System.out.println(s1.length());
+        System.out.println(match(passwordEncryotByMD5,s1));  
+        
+    }  
+ }  
