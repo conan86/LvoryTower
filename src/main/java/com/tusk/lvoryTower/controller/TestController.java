488742ef84a678715fa2c760c2604c668688ce3e
Conan Sun <125001930@qq.com>
2015-11-26 00:52:30 +0800

    add a restful example

--- src/main/java/com/tusk/lvoryTower/controller/TestController.java
+++ src/main/java/com/tusk/lvoryTower/controller/TestController.java
@@ -1,11 +1,19 @@
 package com.tusk.lvoryTower.controller;
 
+import java.io.IOException;
+
 import javax.annotation.Resource;
+
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.tusk.lvoryTower.module.User;
 import com.tusk.lvoryTower.services.UserService;
 
@@ -15,10 +23,20 @@ public class TestController {
 	@Resource
 	private UserService userService;
 
-	@RequestMapping(value = "/getUserInfo", method = RequestMethod.GET)
-	public User getUserInfo(@RequestParam(value = "userId") String userId) {
+	@RequestMapping(value = "/getUserInfo", method = RequestMethod.POST)
+	public User getUserInfo(@RequestParam(value = "userId") String userId) throws JsonProcessingException {
 		long id = Long.parseLong(userId);
 		User user = userService.getUserById(id);
 		return user;
 	}
+	
+	@RequestMapping("getUserInfo2")
+	public User getUserInfo2(@RequestBody String body) throws JsonParseException, JsonMappingException, IOException {
+		ObjectMapper mapper = new ObjectMapper();
+		User user = null;
+		user = mapper.readValue(body,  User.class);
+		user = userService.getUserById(user.getUserId());
+        return user;
+	}
+
 }
